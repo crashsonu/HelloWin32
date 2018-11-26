2c17c6064802bb2a2a3d9958651d84dcbcecc094
crashsonu <crashsonu@gmail.com>
2018-11-26 23:01:57 +0530

    function docstrings updated

--- HelloWin32/functions.c
+++ HelloWin32/functions.c
@@ -3,6 +3,11 @@
 int add(int a, int b)
 /*
 	@ This function will add two values which can be use in any other function.
+	Args:-
+		a (int): first value
+		b (int): second value
+	Return:-
+		c (int): addition of a and b.
 */
 {
 	int c;
@@ -52,3 +57,99 @@ void func_tester_main()
 }
 
 
+int pyramid_using_func(int row)
+/*
+	@ In this function we make pyramid using custom values
+	costom values= "its a changable value" ,we can pass any values related 
+	to its data type while calling function in main function.
+	Here we calling i.e. pyramid_using_func() in the function where its costom value is 5;
+	it returns..
+		*
+	   * *
+	  * * *
+	 * * * *
+	* * * * *
+*/
+{
+	int  column_v, i, j, s, k;
+	column_v = (int)row * 2 - 1;
+	s = (int)row - 1;
+	for (i = 1; i <= row; i++)
+	{
+		for (k = s; k >= 1; k--)
+		{
+			printf(" ");
+		}
+		for (j = 1; j <= i; j++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+		s--;
+	}
+}
+
+
+int digit_pyramid_func(int row)
+/*
+	@ It will print a digit pyramid of given rows.
+*/
+{
+	int i, j, k, l, m = 1;
+	j == row;
+	k = row - 1;
+	for (i = 1; i <= row; i++)
+	{
+		for (l = k; l >= 1; l--)
+		{
+			printf(" ");
+		}
+		for (j = 1; j <= i; j++)
+		{
+			printf("%d ", m);
+		}
+		printf("\n");
+		k--;
+	}
+}
+
+
+int digit_pyramid_func2(int row)
+/*
+	@ It will print table pyramid of given number.
+*/
+{
+	int i, j, c = 1, d, e, u;
+	if (row % 2 != 0)
+	{
+		printf(" enter row in even number in function to get perfect side pyramid \n");
+	}
+	d = row / 2;
+	e = row * d + d;
+	for (i = 1; i <= row; i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			if (c <= e)
+			{
+				printf("%d ", c* row);
+				c++;
+			}
+		}
+		printf("\n");
+	}
+}
+
+
+int simple_table(int my_no)
+/*
+	@ prints simple table of given number.
+*/
+{
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		printf("%d\n", i*my_no);
+	}
+}
